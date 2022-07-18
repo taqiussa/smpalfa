63212e2b06e6b21f342a83b84ca8ac32527a4724
taqiussa <taqiussa@gmail.com>
2022-07-18 08:34:23 +0700

    a

--- resources/views/alquran/print.blade.php
+++ resources/views/alquran/print.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Print Pembayaran Siswa</title>
+    <title>Print Penilaian Alquran</title>
     <style>
         body {
             font-family: 'Times New Roman', Times, serif !important;
